a3caedf27d62df011796170c04edbed56af45511
Gergely Polonkai <gergely@polonkai.eu>
2015-05-04 11:52:24 +0200

    Upgrade packages

--- elpa/buffer-move-0.4/buffer-move-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; buffer-move-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("buffer-move-pkg.el" "buffer-move.el")
-;;;;;;  (21553 16645 222241 807000))
-
-;;;***
-
-(provide 'buffer-move-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; buffer-move-autoloads.el ends here
