4ca025000cfd55d6fa17f6e310edfe50966eb680
Gergely Polonkai <gergely@polonkai.eu>
2015-07-30 13:43:13 +0000

    Refactor Zim timestamp generator as a defun

--- zim.el
+++ zim.el
@@ -1,6 +1,19 @@
+(defun zim-timestamp ()
+  (with-temp-buffer
+    (insert (format-time-string "%Y-%m-%dT%H:%M:%S%z"))
+    (forward-char -2)
+    (insert ":")
+    (forward-char 2)))
+
 (defun insert-zim-timestamp ()
   (interactive)
-  (insert (format-time-string "%Y-%m-%dT%H:%M:%S%z"))
-  (forward-char -2)
-  (insert ":")
-  (forward-char 2))
+  (insert (zim-timestamp)))
+
+(defun insert-zim-header ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (insert
+     (concat "Content-Type: text/x-zim-wiki\n"
+             "Wiki-Format: zim 0.4\n"
+             "Creation-Date: " (zim-timestamp) "\n\n"))))
