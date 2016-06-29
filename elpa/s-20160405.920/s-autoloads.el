1ee823ee922f26ba9732e2d049b36bc5ec29376b
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-06-29 09:21:54 +0200

    Update packages

--- elpa/s-20160405.920/s-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("s.el") (22297 19799 771141 952000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
