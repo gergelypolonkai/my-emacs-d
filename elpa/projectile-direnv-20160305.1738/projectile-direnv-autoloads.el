c48ae18edbc1c9d7c8060e92ea3fdf9561e21f9c
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-09-16 16:37:59 +0200

    Add some new packages

---
+++ elpa/projectile-direnv-20160305.1738/projectile-direnv-autoloads.el
@@ -0,0 +1,16 @@
+;;; projectile-direnv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("projectile-direnv.el") (22490 46091 998412
+;;;;;;  740000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; projectile-direnv-autoloads.el ends here
