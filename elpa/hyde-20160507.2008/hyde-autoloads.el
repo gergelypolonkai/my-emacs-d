2df53f2a7a2b110aa9def712562e817c225b1730
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-09-26 19:37:47 +0200

    Install some more packages

---
+++ elpa/hyde-20160507.2008/hyde-autoloads.el
@@ -0,0 +1,16 @@
+;;; hyde-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("hyde-git.el" "hyde-md.el" "hyde-pkg.el"
+;;;;;;  "hyde.el" "sample-dot-hyde.el") (22505 22510 45690 975000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; hyde-autoloads.el ends here
