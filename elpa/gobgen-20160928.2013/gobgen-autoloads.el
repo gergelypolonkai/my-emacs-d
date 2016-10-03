f00c16522189342e44478407a64bbe0d45d9f3e7
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-10-03 14:29:16 +0200

    Move gobgen installation source to MELPA
    Woo!

---
+++ elpa/gobgen-20160928.2013/gobgen-autoloads.el
@@ -0,0 +1,21 @@
+;;; gobgen-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "gobgen" "gobgen.el" (22514 20100 637300 679000))
+;;; Generated autoloads from gobgen.el
+
+(autoload 'gobgen "gobgen" "\
+Create widgets window for GObject creation
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; gobgen-autoloads.el ends here
---
+++ elpa/gobgen-20160928.2013/gobgen-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gobgen" "20160928.2013" "Generate GObject descendants using a detailed form" '((emacs "24.4")) :keywords '("gobject" "glib" "gtk" "helper" "utilities"))
