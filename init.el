78f6ebc3d3170df6d68f51e8c0ff45a07f531f97
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-10-14 11:12:52 +0200

    Activate avy via use-package

--- init.el
+++ init.el
@@ -409,6 +409,17 @@
   :bind
   (("M-P" . ace-window)))
 
+(use-package avy
+  :demand
+  :config
+  (avy-setup-default)
+  :bind
+  (("C-:" . avy-goto-char)
+   ("C-'" . avy-goto-char-2)
+   ("M-g f" . avy-goto-line)
+   ("M-g w" . avy-goto-word-1)
+   ("M-g e" . avy-goto-word-0)))
+
 ;; Load my own functions
 (load "gnu-c-header.el")
 (load "toggle-window-split.el")
