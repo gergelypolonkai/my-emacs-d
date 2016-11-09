c720e948f766ac5c41aa1c266f5d76b363c172ac
Gergely Polonkai <gergely@polonkai.eu>
2016-11-09 10:14:29 +0100

    Set Hack as the default font

--- customizations.el
+++ customizations.el
@@ -137,6 +137,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "simp" :slant normal :weight normal :height 98 :width normal))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(hl-line ((t (:inherit nil :background "gray25"))))
  '(trailing-whitespace ((t (:inherit nil :background "red1"))))
