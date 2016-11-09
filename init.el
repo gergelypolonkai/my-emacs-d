50bfccd7a8c6af3c8259947349b3c440b52531ce
Gergely Polonkai <gergely@polonkai.eu>
2016-11-09 10:12:53 +0100

    Fix message in gpolonkai/zone-enable

--- init.el
+++ init.el
@@ -165,7 +165,7 @@
 (defun gpolonkai/zone-enable ()
   (interactive)
   (zone-when-idle 60)
-  (message "I will zone out after idling for 60 minutes."))
+  (message "I will zone out after idling for 60 seconds."))
 
 (use-package zone
   :demand
