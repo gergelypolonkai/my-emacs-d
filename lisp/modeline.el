cdafdbf11de58dd39d1e76cd2038f7bf24a93ac8
Gergely Polonkai <gergely@polonkai.eu>
2016-11-21 08:28:41 +0100

    Add a skeleton for redesigning the mode line
    It currently contains the sml-modified default.

---
+++ lisp/modeline.el
@@ -0,0 +1,17 @@
+(setq-default
+ mode-line-format
+ ("%e"
+  mode-line-front-space
+  mode-line-mule-info
+  mode-line-client
+  mode-line-modified
+  mode-line-remote
+  mode-line-frame-identification
+  mode-line-buffer-identification
+  "   "
+  mode-line-position
+  (vc-mode vc-mode)
+  "  "
+  mode-line-modes
+  mode-line-misc-info
+  mode-line-end-spaces))
