97089c4fb7a3bfcec2dc1c6bf442150c52313c23
Gergely Polonkai <gergely@polonkai.eu>
2017-01-05 21:41:48 +0100

    Add Org ELPA to package-archives

--- init.el
+++ init.el
@@ -12,6 +12,8 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
